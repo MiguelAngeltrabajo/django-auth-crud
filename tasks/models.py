abeb0dc9a5c74f9c1c1ee5f2cc44de7996fcfcf8
BarraganMiguel <estsjmabs20049a@gmail.com>
2024-05-19 21:02:25 -0500

    subikk12333jjlllhh999kkk0055777iiippiddi

--- tasks/models.py
+++ tasks/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
+    resena = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
     datecompleted = models.DateTimeField(null=True, blank=True)
     important = models.BooleanField(default=False)
